33878b761d0743186b22d98c8f56b0fcdfdc16b6
T-yopo <samecob62@gmail.com>
2024-01-13 20:50:36 +0000

    I fixed something

---
+++ exitchoice.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+#include "linker.h"
+#include "../calculator 2/linker.h"
+#include "../calculator 2/Var.h"
+#include <string>
+
+using namespace std;
+
+
+void exitchoice(double round) {
+	string choice = "C";
+
+	cout << endl << "\n\n\nType C to choose again, A to do something to your\nlatest result or type anything else to exit: ";
+	cin >> choice;
+	cout << "\n\n\n";
+	if (choice == "C") { returnRound = NULL; main(); }
+	else if (choice == "c") { returnRound = NULL;  main(); }
+	else if (choice == "c") { returnRound = NULL;  main(); }
+
+	else if (choice == "A") { returnReturnRound(round); }
+	else if (choice == "a") { returnReturnRound(round); }
+
+
+
+}
+
