3dc8e1fc486e880df85f1cb9d2e8d13d468d1cf7
T-yopo <samecob62@gmail.com>
2024-02-07 14:01:27 +0000

    Small change to exitchoice.cpp. For more info, please see the changelog from the release.

--- exitchoice.cpp
+++ exitchoice.cpp
@@ -12,14 +12,12 @@ void exitchoice(double round) {
 
 	cout << endl << "\n\n\nType C to choose again, A to do something to your\nlatest result or type anything else to exit: ";
 	cin >> choice;
-	cout << "\n\n\n";
 	if (choice == "C") { returnRound = NULL; main(); }
 	else if (choice == "c") { returnRound = NULL;  main(); }
 	else if (choice == "c") { returnRound = NULL;  main(); }
 
 	else if (choice == "A") { returnReturnRound(round); }
 	else if (choice == "a") { returnReturnRound(round); }
-
 
 
 }
