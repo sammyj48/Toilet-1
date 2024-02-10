9a21fb8bf80a13db36404912e64f979a7dd1fbea
T-yopo <samecob62@gmail.com>
2024-02-10 09:52:49 +0000

    fixed a bug

--- multinumchoice.cpp
+++ multinumchoice.cpp
@@ -6,10 +6,10 @@ void multinumchoice(string param1, double num1, double num2) {
 	char choice;
 	cout << "\n\nDo you want to use the result as your \"latest result\" (R) or the " << param1 << " instead (O)?\n";
 	cin >> choice;
-	if (choice == 'R' or 'r') {
+	if (choice == 'R' || choice == 'r') {
 		exitchoice(num1);
 	}
-	else if (choice == 'O' or '0' or 'o') {
+	else if (choice == 'O' || choice == '0' || choice == 'o') {
 		exitchoice(num2);
 	}
 
